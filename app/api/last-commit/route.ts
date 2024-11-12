1dd609ddc3ee1cc0ea11c857cc50889a04f1eec2
medtty <doussm101@gmail.com>
2024-11-12 09:14:01 +0800

    nextjs15&revalidate

--- app/api/last-commit/route.ts
+++ app/api/last-commit/route.ts
@@ -1,6 +1,8 @@
 import { getLastCommitDate } from '@/utils/getLastCommitDate';
 import { NextResponse } from 'next/server';
 
+export const revalidate = 10; // Revalidate every 10 seconds
+
 export async function GET() {
   const date = getLastCommitDate();
   return NextResponse.json({ date });
